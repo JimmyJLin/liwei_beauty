09707448cfee55d943d1bec25445b590ebc695a4
Jimmy Lin <usmcjimmy@gmail.com>
2016-05-13 21:09:38 -0400

    initial express with semantic-ui

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+/* app setting */
+const port = process.env.PORT || 3000;
+const app = express();
+const server = app.listen(port);
+
+// parse incoming forms
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// static route to public
+app.use(express.static(path.join(__dirname, './public/')));
+
+// log
+app.use(logger('dev'));
+
+/*Views*/
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+/* Routes */
+// Landing Page
+app.get('/', function(req, res){
+  res.render('./pages/index')
+})
