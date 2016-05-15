6d5fc382d910b78e1e3aa19f2425584c387af434
Jimmy Lin <usmcjimmy@gmail.com>
2016-05-15 16:28:33 -0400

    changed social media floating display

--- public/services/js/script.js
+++ public/services/js/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  console.log("Service script.js")
+  console.log("Regular script.js")
   // Dropdown Menu for Services
   $('.dropdown').dropdown()
 
@@ -7,65 +7,27 @@ $(function(){
   $('#m_btn').on('click', function(){
     $('#m_menu').sidebar('toggle');
   })
-  //
-  // $('#fullpage').fullpage({
-  //   //Navigation
-  //   menu: '#menu',
-  //   lockAnchors: false,
-  //   anchors:['firstPage', 'secondPage'],
-  //   navigation: false,
-  //   navigationPosition: 'right',
-  //   navigationTooltips: ['firstSlide', 'secondSlide'],
-  //   showActiveTooltip: false,
-  //   slidesNavigation: true,
-  //   slidesNavPosition: 'bottom',
-  //
-  //   //Scrolling
-  //   css3: true,
-  //   scrollingSpeed: 700,
-  //   autoScrolling: true,
-  //   fitToSection: true,
-  //   fitToSectionDelay: 1000,
-  //   scrollBar: false,
-  //   easing: 'easeInOutCubic',
-  //   easingcss3: 'ease',
-  //   loopBottom: false,
-  //   loopTop: false,
-  //   loopHorizontal: true,
-  //   continuousVertical: false,
-  //   normalScrollElements: '#element1, .element2',
-  //   scrollOverflow: false,
-  //   touchSensitivity: 15,
-  //   normalScrollElementTouchThreshold: 5,
-  //
-  //   //Accessibility
-  //   keyboardScrolling: true,
-  //   animateAnchor: true,
-  //   recordHistory: true,
-  //
-  //   //Design
-  //   controlArrows: true,
-  //   verticalCentered: true,
-  //   resize : false,
-  //   sectionsColor : ['#ccc', '#fff'],
-  //   paddingTop: '3em',
-  //   paddingBottom: '10px',
-  //   fixedElements: '#header, .footer',
-  //   responsiveWidth: 0,
-  //   responsiveHeight: 0,
-  //
-  //   //Custom selectors
-  //   sectionSelector: '.section',
-  //   slideSelector: '.slide',
-  //
-  //   //events
-  //   onLeave: function(index, nextIndex, direction){},
-  //   afterLoad: function(anchorLink, index){},
-  //   afterRender: function(){},
-  //   afterResize: function(){},
-  //   afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-  //   onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-  //
-  // })
+
+
+  // fix main menu to page on passing
+  $('.main.menu').visibility({
+    type: 'fixed'
+  });
+  $('.overlay').visibility({
+    type: 'fixed',
+    offset: 80
+  });
+
+  // lazy load images
+  $('.image').visibility({
+    type: 'image',
+    transition: 'vertical flip in',
+    duration: 500
+  });
+
+  // show dropdown on hover
+  $('.main.menu  .ui.dropdown').dropdown({
+    on: 'hover'
+  });
 
 })
